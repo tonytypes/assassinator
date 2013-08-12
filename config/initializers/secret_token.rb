439b8f68f8a52751aee5581eab807e6182d24480
Tony Ruiz <tonytypes@gmail.com>
2013-08-11 22:32:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewsion::Application.config.secret_token = '96b9db3e3ee2e7c928d0cc5a3abf07bdf1cf7d540349d3b1f666c344dc0a7d4242be51e83a21d62cd84349ffac3224fd07f55e64eb81dd96526f3213df64cb2d'
