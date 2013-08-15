bea81d1d93fe772c9991573e0f59cfc610c446ab
Tony Ruiz <tonytypes@gmail.com>
2013-08-15 08:25:14 -0400

    Add and customize bootstrap, devise, and simple_form, and temporarily disabled password recovery

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 group :production do
 	gem 'pg'
@@ -15,4 +17,5 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.3.2.1'
 end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         # Temporarily removed ability to recover password
+         # via email. That link no longer appears on any pages
+         #:recoverable,
+         :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  # attr_accessible :title, :body
+end
